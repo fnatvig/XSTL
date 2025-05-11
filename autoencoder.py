35dd65e4e3db1ac4e94544df714d2f1902ab847f
Filip Natvig <filna740@user.uu.se>
2025-05-11 08:36:41 +0200

    Working version

--- autoencoder.py
+++ autoencoder.py
@@ -7,40 +7,52 @@ from keras.layers import Dense, Input, Dropout
 import tensorflow as tf
 from joblib import Parallel, delayed
 from scipy import integrate
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
 
 def train(data, epochs, patience, lr):
 
     model = Sequential(
         [
         Input(shape=(len(data.columns), ),),
-        Dropout(0.4),
-        Dense(int(len(data.columns)), activation='relu'),
-        Dropout(0.4),
-        Dense(int(len(data.columns)*0.5), activation='relu'),
-        Dropout(0.2),
+        # Dense(int(len(data.columns)*4), activation='relu'),
+        # Dropout(0.4),
+        # Dense(int(len(data.columns)*3), activation='relu'),
+        # Dropout(0.4),
+        # Dense(int(len(data.columns)*2), activation='relu'),
+        # Dropout(0.4),
+        Dropout(0.1),
+        Dense(32, activation='relu'),
+        Dropout(0.1),
+        Dense(16, activation='relu'),
+        Dropout(0.1),
+        Dense(8, activation='relu'),
+        Dropout(0.1),
         Dense(2, activation='relu', name='bottleneck'),
-        Dense(int(len(data.columns)*0.5), activation='relu'),
-        Dense(int(len(data.columns)), activation='relu', name='last_hidden'),
-        Dense(len(data.columns), activation='sigmoid')
+        Dropout(0.1),
+        Dense(8, activation='relu'),
+        Dropout(0.1),
+        Dense(16, activation='relu'),
+        Dropout(0.1),
+        Dense(32, activation='relu'),
+        Dropout(0.1),
+
+        # Dense(int(len(data.columns)*2), activation='relu', name='last_hidden'),
+        # Dense(int(2*len(data.columns)*3), activation='relu', name='last_last_hidden'),
+        # Dense(int(2*len(data.columns)*4), activation='relu', name='last_last_last_hidden'),
+        Dense(len(data.columns), activation='linear')
         ]
     )
-    
     early_stopping = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=patience, mode="min")
     opt = tf.keras.optimizers.Adam(learning_rate=lr)
     model.compile(optimizer=opt, loss='mae')
     
-    history= model.fit(data.values, data.values, epochs=epochs, batch_size=256, callbacks=[early_stopping], shuffle=True, verbose=0)
+    history= model.fit(data.values, data.values, epochs=epochs, batch_size=32, callbacks=[early_stopping], shuffle=True, verbose=0)
 
     return model
 
 def mse(arr1, arr2):
-    mse = []
-    for i in range(arr1.shape[0]):
-        timestep_err = 0
-        for j in range(arr1.shape[1]):
-            timestep_err += (arr1[i][j]-arr2[i][j])*(arr1[i][j]-arr2[i][j])
-        mse.append(timestep_err/arr1.shape[1])
-    return np.array(mse)
+    return np.mean((arr1 - arr2) ** 2, axis=1)
 
 def retrain(data, model, epochs, patience, trainable_encoder, lr, return_threshold=False):
     if not trainable_encoder:
@@ -67,7 +79,7 @@ def retrain(data, model, epochs, patience, trainable_encoder, lr, return_thresho
     early_stopping = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=patience, mode="min")
     opt = tf.keras.optimizers.Adam(learning_rate=lr)
     model.compile(optimizer=opt, loss='mae')
-    history= model.fit(data.values, data.values, epochs=epochs, batch_size=256, callbacks=[early_stopping], shuffle=True, verbose=0)
+    history= model.fit(data.values, data.values, epochs=epochs, batch_size=32, callbacks=[early_stopping], shuffle=True, verbose=0)
     
     if return_threshold:
         y_hat = model.predict(data.values, verbose=False)
@@ -77,57 +89,16 @@ def retrain(data, model, epochs, patience, trainable_encoder, lr, return_thresho
 
     return model
 
-def compute_tpr_fpr(threshold, err, true_indexes_set):
-    """ Compute TPR and FPR for a given threshold. """
-    tp, fp, fn, tn = 0, 0, 0, 0
-    for i in range(len(err)):
-        if err[i] >= threshold:
-            if i in true_indexes_set:
-                tp += 1
-            else:
-                fp += 1
-        else:
-            if i in true_indexes_set:
-                fn += 1
-            else:
-                tn += 1
-    fpr = fp / (fp + tn) if (fp + tn) > 0 else 0
-    tpr = tp / (tp + fn) if (tp + fn) > 0 else 0
-    return threshold, fpr, tpr  # Returning threshold for sorting
-
-def parallel_auc(err, true_indexes_array, thresholds, n_jobs=-1):
-    """ Compute AUC using parallelization over thresholds. """
-    true_indexes_set = set(true_indexes_array)  # Convert to set for faster lookup
-
-    # Parallel computation
-    results = Parallel(n_jobs=n_jobs)(
-        delayed(compute_tpr_fpr)(threshold, err, true_indexes_set) for threshold in thresholds
-    )
-
-    # Sorting by threshold in descending order (to match original reverse logic)
-    results.sort(reverse=True, key=lambda x: x[0])
-    
-    # Extract sorted FPR and TPR values
-    fpr = [res[1] for res in results]
-    tpr = [res[2] for res in results]
-
-    # Compute AUC using trapezoidal integration
-    return integrate.trapezoid(y=tpr, x=fpr)
-
 def reconstruct_auc(prepared_df, data, model1, model2, num_thresholds):
     y_hat1 = model1.predict(prepared_df.values, verbose=False)
     err1 = mse(y_hat1, prepared_df.values)
 
     y_hat2 = model2.predict(prepared_df.values, verbose=False)
     err2 = mse(y_hat2, prepared_df.values)
-
-    temp = data[data.loc[:, "label"]].index
-    true_indexes_array = temp.to_numpy(dtype='int')
-
-    thresholds = np.linspace(0, max([*err1, *err2]), num_thresholds)
-
-    auc1 = parallel_auc(err1, true_indexes_array, thresholds)
-    auc2 = parallel_auc(err2, true_indexes_array, thresholds)
+    
+    auc1 = roc_auc_score(data.loc[:,"label"], err1)
+    auc2 = roc_auc_score(data.loc[:,"label"], err2)
+    
     return auc1, auc2
 
 def get_threshold(err, true_indexes_array):
@@ -149,7 +120,51 @@ def get_fpr(err, true_indexes_array):
                 tn+=1
     return fp/(fp+tn)
 
-def reconstruct_sensitive(data, data_raw, model1, model2):
+def get_fpr_at_fixed_tpr(y_true, y_score, tpr_targets=[1.0, 0.95], tolerance=1e-4):
+    fpr, tpr, thresholds = roc_curve(y_true, y_score)
+    results = []
+
+    for tpr_target in tpr_targets:
+        mask = tpr >= (tpr_target - tolerance)
+        if not any(mask):
+            # results[tpr_target] = {'fpr': None, 'threshold': None}
+            results.append(None)
+        else:
+            best_idx = mask.nonzero()[0][fpr[mask].argmin()]
+            results.append(fpr[best_idx])
+    return results
+
+def get_tpr_at_fixed_fpr(y_true, y_score, fpr_targets=[0.05], tolerance=1e-4):
+    fpr, tpr, thresholds = roc_curve(y_true, y_score)
+    results = []
+
+    for fpr_target in fpr_targets:
+        # Find indices where FPR is just below or equal to the target (within tolerance)
+        mask = fpr <= (fpr_target + tolerance)
+        if not any(mask):
+            results.append(None)
+        else:
+            best_idx = mask.nonzero()[0][-1]  # max TPR under FPR constraint
+            results.append(tpr[best_idx])
+    return results
+
+def detect(data, data_raw, model_a, model_b, tpr_target):
+    y_hat1 = model_a.predict(data.values, verbose=False)
+    err1 = mse(y_hat1, data.values)
+
+    y_hat2 = model_b.predict(data.values, verbose=False)
+    err2 = mse(y_hat2, data.values)
+
+    fpr_a = get_fpr_at_fixed_tpr(data_raw.loc[:,"label"], err1, tpr_target)
+    fpr_b = get_fpr_at_fixed_tpr(data_raw.loc[:,"label"], err2, tpr_target)
+
+    auc_a = roc_auc_score(data_raw.loc[:,"label"], err1)
+    auc_b = roc_auc_score(data_raw.loc[:,"label"], err2)
+
+    return fpr_a[0], fpr_b[0], auc_a, auc_b
+    
+
+def reconstruct_sensitive(data, data_raw, model1, model2, tpr_target, fpr_target):
     y_hat1 = model1.predict(data.values, verbose=False)
     err1 = mse(y_hat1, data.values)
 
@@ -159,7 +174,18 @@ def reconstruct_sensitive(data, data_raw, model1, model2):
     temp = data_raw[data_raw.loc[:, "label"]].index
     true_indexes_array = temp.to_numpy(dtype='int')
 
-    fpr1 = get_fpr(err1, true_indexes_array)
-    fpr2 = get_fpr(err2, true_indexes_array)
-    
-    return fpr1, fpr2
+    # fpr1 = get_fpr(err1, true_indexes_array)
+    # fpr2 = get_fpr(err2, true_indexes_array)
+
+    results1 = get_fpr_at_fixed_tpr(data_raw.loc[:,"label"], err1, tpr_target)
+    results2 = get_fpr_at_fixed_tpr(data_raw.loc[:,"label"], err2, tpr_target)
+
+    results3 = get_tpr_at_fixed_fpr(data_raw.loc[:,"label"], err1, fpr_target)
+    results4 = get_tpr_at_fixed_fpr(data_raw.loc[:,"label"], err2, fpr_target)
+
+    auc1 = roc_auc_score(data_raw.loc[:,"label"], err1)
+    auc2 = roc_auc_score(data_raw.loc[:,"label"], err2)
+
+    print("AUC_diff = ", auc1-auc2)
+
+    return results1, results2, results3, results4
--- run_tests.bat
+++ run_tests.bat
@@ -1,5 +1,5 @@
 @echo off
 call venv\Scripts\activate.bat
-python -B run_experiment.py --test B1
+python -B run_experiment.py --test C1
 
 pause
